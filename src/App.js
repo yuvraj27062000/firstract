01de08b0d398bf3150f3bb346947254ff8c4a134
yuvraj <chouhanyuvrajsingh83@gmail.com>
2022-11-09 21:00:39 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <p>I am learning React. My life is getting better. </p>
+    </div>
+  );
+}
+
+export default App;
