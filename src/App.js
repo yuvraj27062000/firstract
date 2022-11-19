671b9c27a76448d8d243966010019c521e428a47
yuvraj <chouhanyuvrajsingh83@gmail.com>
2022-11-19 23:48:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
-import  Firstapp  from './component/Firstapp';
+// import  Firstapp  from './component/Firstapp';
+import Counter from './component/Counter';
 function App() {
   return (
     <div className="App">
-        <Firstapp />
+        {/* <Firstapp /> */}
+        <Counter/>
     </div>
   );
 }
---
+++ src/component/Counter.jsx
@@ -0,0 +1,36 @@
+import {React, useState, useEffect} from "react";
+
+// const[count, setCount] = useState(0);
+const Counter = ()=>{
+    let data = false
+    const[count, setCount] = useState(1);
+    const[show, shoeSpan] = useState(false);
+    function add(){
+        setCount(count+1);
+    }
+    function sub(){
+       
+        if(count>0){
+        setCount(count-1);
+        }
+      
+    }
+    useEffect(()=>{
+        
+        if(count==0){
+            shoeSpan(false)
+        }
+        else{
+            shoeSpan(true)
+        }
+    },[count])
+    return(
+        <div>
+            <h1>{count}</h1>
+            <button onClick={add}>+</button>
+            <button onClick={sub}>-</button><br />
+            {!show && <span>It connot be DECREASE </span>}
+        </div>
+    )
+}
+export default Counter
